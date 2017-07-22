8dea71224c0ae1a92178ee611f3f4d187095fd0b
KamilMosakowski <kam.mosak@gmail.com>
2017-07-22 14:20:44 +0200

    majestatyczna planeta

--- main.lua
+++ main.lua
@@ -4,7 +4,11 @@ local tlo = display.newImage( "images/bg.png", display.contentCenterX, display.c
 tlo.width = display.actualContentWidth
 tlo.height = display.actualContentHeight
 
+tlo.fill.effect = "filter.blur"
+
 local planeta = display.newImage( "images/planeta.png", 250, 150 )
+planeta.xScale = 2
+planeta.yScale = 2
 
 local przeciwnik = display.newImage( "images/ufo.png", 400, 200 )
 
@@ -12,12 +16,24 @@ local statek = display.newImage( "images/statek.png", 100, 100 )
 
 local function ruszajObiektami()
 
-	planeta.x = planeta.x - 10
+	--planeta
+	
+	planeta.x = planeta.x - 1
+	
+	if planeta.x < - 150 then 
+		planeta.x = display.actualContentWidth + 150 
+	end
 
-    	if planeta.x < - 150 then 
-            planeta.x = display.actualContentWidth + 150 
-        end
+	-- statek przeciwnika
+	
+	przeciwnik.x = przeciwnik.x - 3
+	
+	if przeciwnik.x < - 100 then 
+		przeciwnik.x = display.actualContentWidth + 100 
+        przeciwnik.y = math.random( 50, display.actualContentHeight - 50 )
+	end
 
+	
 end
 
 Runtime:addEventListener( "enterFrame", ruszajObiektami )
