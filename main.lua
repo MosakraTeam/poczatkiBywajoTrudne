e95e28421160adf46555df601073e59886b709d9
KamilMosakowski <kam.mosak@gmail.com>
2017-07-22 14:16:33 +0200

    ziuu

--- main.lua
+++ main.lua
@@ -1,7 +1,23 @@
-local x = 1
+display.setStatusBar( display.HiddenStatusBar )
+local tlo = display.newImage( "images/bg.png", display.contentCenterX, display.contentCenterY )
 
-if x < 1 then x = 0 end
+tlo.width = display.actualContentWidth
+tlo.height = display.actualContentHeight
 
-print( x )
+local planeta = display.newImage( "images/planeta.png", 250, 150 )
 
-local napis = "Koniec gry"
+local przeciwnik = display.newImage( "images/ufo.png", 400, 200 )
+
+local statek = display.newImage( "images/statek.png", 100, 100 )
+
+local function ruszajObiektami()
+
+	planeta.x = planeta.x - 10
+
+    	if planeta.x < - 150 then 
+            planeta.x = display.actualContentWidth + 150 
+        end
+
+end
+
+Runtime:addEventListener( "enterFrame", ruszajObiektami )
